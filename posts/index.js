b2ce73ea20b9deb93cbdbda572d36aada2cd2f02
Qasim Hafeez <qasimh556@gmail.com>
2020-08-26 12:23:46 +0400

    made a boiler plate

---
+++ posts/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const { randomBytes } = require("crypto");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
+
+const posts = {};
+
+// Routes
+app.get("/posts", (req, res) => {
+  res.send(posts);
+});
+
+app.post("/posts", (req, res) => {
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
+
+  posts[id] = {
+    id,
+    title,
+  };
+
+  res.status(201).send(posts[id]);
+});
+
+// Server Running
+app.listen(5000, () => {
+  console.log("Server is running on PORT: 4000");
+});
